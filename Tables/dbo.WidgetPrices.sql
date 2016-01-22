1df2b266aaf8c3047ff2dfb2768f6eef88f0ab03
bbrown127 <bbrown@LOCAL>
2016-01-22 10:17:15 -0600

    Test adding a flag column

--- Tables/dbo.WidgetPrices.sql
+++ Tables/dbo.WidgetPrices.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[WidgetPrices]
 (
 [RecordID] [int] NOT NULL IDENTITY(1, 1),
 [WidgetID] [int] NULL,
-[Price] [money] NULL
+[Price] [money] NULL,
+[Flag] [bit] NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[WidgetPrices] ADD CONSTRAINT [PK_WidgetPrices] PRIMARY KEY NONCLUSTERED  ([RecordID]) ON [PRIMARY]
